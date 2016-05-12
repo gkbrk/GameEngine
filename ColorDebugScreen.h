1f332980ef203c5f18e15c112b25a95fe24c8e7a
Gökberk Yaltıraklı <webdosusb@gmail.com>
2016-05-12 23:36:04 +0300

    Added more generic ColorDebugScreen and replaced RedScreen with it

---
+++ ColorDebugScreen.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "GameState.h"
+#include "Engine.h"
+#include "Mouse.h"
+#include <iostream>
+
+class ColorDebugScreen: public GameState {
+    public:
+        ColorDebugScreen() {
+            red = 255;
+            green = 0;
+            blue = 0;
+        }
+
+        ColorDebugScreen(int r, int g, int b) {
+            red = r;
+            green = g;
+            blue = b;
+        }
+
+        void Draw() {
+            SDL_FillRect(Engine::surface, NULL, SDL_MapRGB(Engine::surface->format, red, green, blue));
+        }
+
+        void Update(double dt) {
+            std::cout << Mouse::x << "|" << Mouse::y << std::endl;
+        }
+
+        void SetColor(int r, int g, int b) {
+            red = r;
+            green = g;
+            blue = b;
+        }
+
+        int red, green, blue;
+};
