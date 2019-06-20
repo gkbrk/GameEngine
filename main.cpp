4d6d14972270b9772b16337c94a89b9cce9e1c13
Fatih Melih Özgün <fedqx@hotmail.com>
2019-06-20 22:12:57 +0300

    Added comments

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     Engine::init_sdl();
     Engine::gameStates.push_back(new SplashScreen("Resources/splash/", 2, 500));
     Engine::gameStates.push_back(new DebugScreen());
-    
+    // Main loop
     while (true) {
         if (!Engine::tick()) {
             break;
